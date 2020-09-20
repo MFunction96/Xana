93b08938d4c2d26d0f1da9024fdd5560c755a66d
mfunction <mfunction96@gmail.com>
2020-09-20 23:00:27 +0800

    Add boost test feature

---
+++ src/test/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by MFunction on 09/20/2020.
+//
+
+#define BOOST_TEST_MODULE XSHELLTEST
+
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(BoostTest_Checkin) {
+	auto i = 1;
+	BOOST_TEST(i == 1);
+	BOOST_TEST_MESSAGE("Boost Test checked in successfully.");
+}
